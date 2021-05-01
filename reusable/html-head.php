e249f346fe02da83dc20b03b4e88558582daac0c
gromanaz <gromanaz@users.noreply.github.com>
2021-05-02 01:47:26 +0200

    extracted reusable code (could be used for php includes)

---
+++ reusable/html-head.php
@@ -0,0 +1,7 @@
+<head>
+  <meta charset="utf-8">
+  <title>DMS 290 - Project 4 - Team San Francisco</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Euphoria+Script&family=Quattrocento&display=swap" rel="stylesheet">
+</head>
---
+++ reusable/page-footer.php
@@ -0,0 +1,4 @@
+<footer>
+  <div>Project for DMS 290 - Front-End Web Design</div>
+  <div>2021 - Bradley Martin & Gina Romanazzi</div>
+</footer>
